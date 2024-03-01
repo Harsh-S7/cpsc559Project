53bafdad0a7c16c69ba2a39a48a2d7fc48242307
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2024-02-29 20:17:06 -0700

    feat: poc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,35 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { seedString } from "./definitions/placeholder.definition";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
-const hello = (req: Request, res: Response) => {
-  res.send({m:"Express + TypeScript Server"});
-};
+var mstring = seedString
 
-app.get("/", hello);
+const getString = (_: Request, res: Response) => {
+  res.send({m:mstring})
+}
+
+const insertString = (req:Request, res: Response) => {
+  const b = req.body
+  mstring = mstring.slice(0, b.index) + b.string + mstring.slice(b.index);
+  res.status(200).send();
+}
+
+const removeString = (req:Request, res: Response) => {
+  const b = req.body
+  mstring = mstring.slice(0, b.fromIndex) + mstring.slice(b.toIndex);
+  res.status(200).send();
+}
+
+app.get("/mstring/", getString);
+app.post("/insertString/", insertString);
+app.post("/removeString/", removeString);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
