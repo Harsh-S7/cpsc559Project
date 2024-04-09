88d8f063b6fa38aa277837f5e6d8fd945e985966
Viet An Truong <vietan124@gmail.com>
2024-04-08 21:05:59 -0600

    remove vite config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3003',
+                target: 'http://proxy:3000',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '/api')
             },
