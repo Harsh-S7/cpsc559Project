e5c588a176ec80fac0dcccd048a2b9e4853ae808
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2024-04-09 00:19:30 -0600

    basic logger

--- http-server/src/index.ts
+++ http-server/src/index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import { UserController } from "./modules/user/user.controller";
 import { mongdoDbSetup } from "./utils/mongodb.utils";
 import { DocumentController } from "./modules/document/document.controller";
+import { basicLogger } from "./utils/logger.utils";
+import { errorHandler } from "./utils/error.utils";
 
 const BASE_URL = "/api";
 
@@ -13,6 +15,8 @@ const app: Express = express();
 const port = process.env.PORT || 3003;
 app.use(express.json());
 
+app.use(basicLogger);
+//app.use(errorHandler);
 //load modules
 app.use(`${BASE_URL}/user`, UserController);
 app.use(`${BASE_URL}/document`, DocumentController);
