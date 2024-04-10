09b2e75924fef1ed2c2973da56f2602bbe0dcc4f
Harsh Sharma <sharma.harsh7111@gmail.com>
2024-04-09 21:00:59 -0600

    Add comments

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
+// Load environment variables from .env file
 require("dotenv").config();
+
+// Import necessary modules
 const http = require("http");
 const express = require("express");
 const WebSocketServer = require("ws").Server;
@@ -8,10 +11,17 @@ const { setPersistence, setupWSConnection } = require("../websocket/utils.js");
 const WebSocket = require("ws");
 const axios = require("axios");
 const { MongoClient, ObjectId, Binary } = require("mongodb");
+
+// Define heartbeat interval for leader checks
 const heartbeatInterval = 15000; // 15 seconds
+
+// Election timeout variable, will be used to determine leader election timing
 let electionTimeout = null;
+
+// Array to store the nodes in the distributed system
 const nodes = [];
 
+// Parse and add nodes from environment variables to the nodes array
 function parseNodesFromEnv() {
   let i = 1;
   while (true) {
@@ -31,8 +41,10 @@ function parseNodesFromEnv() {
   }
 }
 
+// Parse nodes at startup
 parseNodesFromEnv();
 
+// Variables to keep track of the node's status in the election
 let isPrimary = false;
 let primaryId = null;
 let heartbeatTimeout = null;
@@ -40,29 +52,28 @@ let heartbeatTimeout = null;
 // Create an instance of Express
 const app = express();
 
-// API: login user, logout, share documents, getCurrentUsersDocuments
-
-// Use Express to handle JSON and URL-encoded form data
+// Middleware to handle JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Create an HTTP server and pass the Express app
+// Create an HTTP server with the Express app
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 
 console.log("PORT: ", PORT);
-const myId = PORT; // Node ID is set in the environment variable
+const myId = PORT; // Use the port as a unique identifier for this node
 
-// Existing code for WebSocket server setup
+// Setup WebSocket server for real-time communication
 const wss = new WebSocketServer({ server });
-wss.on("connection", setupWSConnection);
+wss.on("connection", setupWSConnection); // Setup WebSocket connection handler
 console.log("WebSocket server is listening");
 
-// y-mongodb-provider setup
+// MongoDB setup with y-mongodb-provider for Yjs documents
 if (!process.env.MONGO_URL) {
   throw new Error("Please define the MONGO_URL environment variable");
 }
 
+// MongoDB client setup
 const client = new MongoClient(process.env.MONGO_URL);
 const mdb = new MongodbPersistence(
   process.env.MONGO_URL + "/" + process.env.DATABASE,
@@ -73,24 +84,26 @@ const mdb = new MongodbPersistence(
   },
 );
 
+// Set up persistence for Yjs documents using MongoDB
 setPersistence({
   bindState: async (docName, ydoc) => {
     const persistedYdoc = await mdb.getYDoc(docName);
-    const newUpdates = Y.encodeStateAsUpdate(ydoc); // Insert or Delete
-    mdb.storeUpdate(docName, newUpdates); // Stores it in the datbase
-    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc)); // applies the db to the doc on the server
+    const newUpdates = Y.encodeStateAsUpdate(ydoc); // Get updates for document
+    mdb.storeUpdate(docName, newUpdates); // Store updates in MongoDB
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc)); // Apply updates from MongoDB to the document
     ydoc.on("update", async (update) => {
       console.log(update);
-      mdb.storeUpdate(docName, update);
-    }); // Listens for updates and stores them in the db
+      mdb.storeUpdate(docName, update); // Listen for updates and store them
+    });
   },
   writeState: () => {
     return new Promise((resolve) => {
-      resolve(true);
-    }); // This promise sends the state to all the clients
+      resolve(true); // This simulates sending the state to all clients
+    });
   },
 });
 
+// Function to start the election process
 function startElection() {
   isPrimary = false;
   primaryId = null;
@@ -103,18 +116,19 @@ function startElection() {
         .post(`${node.address}/election`, { from: myId })
         .catch((e) => console.log(`Node ${node.id} is down`));
     });
-    // Set a timeout to become the leader if no one responds
-    electionTimeout = setTimeout(becomeLeader, 5000); // 5 seconds timeout for simplicity
+    // Set a timeout to become the leader if no response
+    electionTimeout = setTimeout(becomeLeader, 5000); // 5 seconds to wait for responses
   }
 }
 
+// Function to make the current node a leader
 function becomeLeader() {
   if (!isPrimary) {
     isPrimary = true;
     primaryId = myId;
     console.log(`Node ${myId} is now the leader`);
     clearTimeout(heartbeatTimeout);
-    // Announce leadership to all nodes
+    // Announce new leader status to all nodes
     nodes.forEach((node) => {
       if (node.id != myId) {
         axios
@@ -125,7 +139,7 @@ function becomeLeader() {
   }
 }
 
-// Reset leader if a new leader is announced
+// Endpoint to handle new leader announcements
 app.post("/new-leader", (req, res) => {
   const { leaderId } = req.body;
   clearTimeout(electionTimeout);
@@ -135,17 +149,17 @@ app.post("/new-leader", (req, res) => {
   res.send("Leader acknowledged");
 });
 
+// Endpoint to handle election messages
 app.post("/election", (req, res) => {
   const { from } = req.body;
   console.log(`Election message received from ${from}`);
-  // Respond to signal active status and start own election
   res.send("OK");
   startElection();
 });
 
+// Send heartbeat from the leader to all nodes periodically
 setInterval(() => {
   if (isPrimary) {
-    // Leader sends heartbeat to all
     nodes.forEach((node) => {
       if (node.id != myId) {
         axios
@@ -154,18 +168,17 @@ setInterval(() => {
       }
     });
   }
-}, 5000); // 10 seconds for simplicity
+}, 5000); // Every 5 seconds
 
+// Endpoint to handle heartbeats from the leader
 app.post("/heartbeat", (req, res) => {
   const { leaderId } = req.body;
-  // Reset any election timeout upon receiving a heartbeat
   clearTimeout(electionTimeout);
   clearTimeout(heartbeatTimeout); // Reset the heartbeat timeout
 
   primaryId = leaderId;
   console.log(`Received heartbeat from leader ${leaderId}`);
 
-  // Restart the heartbeat timeout
   heartbeatTimeout = setTimeout(() => {
     console.log("Heartbeat missed. Starting election.");
     startElection();
@@ -174,29 +187,27 @@ app.post("/heartbeat", (req, res) => {
   res.send("Heartbeat acknowledged");
 });
 
+// Endpoint to check if the node is primary
 app.get("/isPrimary", (req, res) => {
   console.log("IS PRIMARY: ", isPrimary);
   res.json({ isPrimary: isPrimary });
 });
 
+// Synchronize documents across nodes periodically if this node is the primary
 setInterval(async () => {
   if (isPrimary) {
-    // Fetch and forward documents only if this node is the primary
     try {
       await client.connect();
       const collection = client
         .db(process.env.DATABASE)
         .collection(process.env.COLLECTION_NAME);
       const documents = await collection.find({}).toArray();
-      // Forward documents to all replica nodes
       nodes.forEach((node) => {
         if (node.id != myId) {
           axios
             .post(`${node.address}/document-sync`, { documents })
             .catch((e) =>
-              console.log(
-                `Failed to forward documents to Node ${node.id}: ${e.message}`,
-              ),
+              console.log(`Failed to forward documents to Node ${node.id}: ${e.message}`),
             );
         }
       });
@@ -204,8 +215,9 @@ setInterval(async () => {
       console.error("Failed to fetch or forward documents:", e.message);
     }
   }
-}, 3000);
+}, 3000); // Every 3 seconds
 
+// Endpoint for document synchronization between nodes
 app.post("/document-sync", async (req, res) => {
   const { documents } = req.body;
 
@@ -215,19 +227,15 @@ app.post("/document-sync", async (req, res) => {
       .db(process.env.DATABASE)
       .collection(process.env.COLLECTION_NAME);
 
-    // Delete all documents. Consider implications of temporary data unavailability.
-    await collection.deleteMany({});
+    await collection.deleteMany({}); // Delete all documents before synchronization
 
-    // Insert the new documents if any
     if (documents.length > 0) {
-      // Map documents to retain original _id values
-      console.log(documents);
       const modifiedDocuments = documents.map((doc) => ({
         ...doc,
-        value: doc.value ? Binary.createFromBase64(doc.value, 0) : null, // Convert value to Binary
-        _id: doc._id ? new ObjectId(doc._id) : new ObjectId(), // Retain original _id or generate new ObjectId
+        value: doc.value ? Binary.createFromBase64(doc.value, 0) : null,
+        _id: doc._id ? new ObjectId(doc._id) : new ObjectId(),
       }));
-      await collection.insertMany(modifiedDocuments);
+      await collection.insertMany(modifiedDocuments); // Insert synchronized documents
     }
 
     console.log(`Synchronized ${documents.length} documents.`);
@@ -238,6 +246,7 @@ app.post("/document-sync", async (req, res) => {
   }
 });
 
+// Update the primary node's address to a proxy or another service periodically
 setInterval(async () => {
   if (isPrimary) {
     let PrimaryAddress = "";
@@ -252,12 +261,12 @@ setInterval(async () => {
       }
     });
   }
-}, 5000);
+}, 5000); // Every 5 seconds
 
+// Start the server and initialize leader election and heartbeat timeout mechanisms
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-  startElection(); // Start an election when the server starts
-  // Initialize heartbeat timeout for replicas
+  startElection(); // Initiate election process at server start
   if (!isPrimary) {
     heartbeatTimeout = setTimeout(() => {
       console.log("Heartbeat missed at startup. Starting election.");
