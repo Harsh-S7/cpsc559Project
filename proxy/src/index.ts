cbd7d4d22adb436f8bcec176e728fd93bb28554e
Hy Huynh <giahy33@gmail.com>
2024-04-09 21:33:22 -0600

    feat: document Proxy

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,24 +1,21 @@
-// src/index.js
 import express, { Express, Request, Response, NextFunction, RequestHandler } from "express";
 import dotenv from "dotenv";
 import proxy from "express-http-proxy";
 import expressWs from 'express-ws';
 import WebSocket from 'ws';
 import cors from "cors";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 const { app: wsApp } = expressWs(app);
-let webSocket: WebSocket | null = null;
-let reconnectTimeout: NodeJS.Timeout | null = null;
 let primaryServerUrl: string = '';
-// Primary server user URL
 let primaryServerUserUrl: string = 'https://shaggy-symbols-laugh.loca.lt/';
 let actualWebSocket: WebSocket;
 
+
+// Map of Editor Server url to the corresponding User Server url
 let serverUrlMap: Record<string, string> = {
   'https://wild-banks-rush.loca.lt/': 'https://shaggy-symbols-laugh.loca.lt/',
   'https://deep-tigers-push.loca.lt/': 'https://smooth-taxes-fold.loca.lt/',
@@ -28,12 +25,15 @@ let serverUrlMap: Record<string, string> = {
 // Function to create a WebSocket connection to the primary or secondary server
 const connectToWebSocketServer = (documentNumber: string, ws: WebSocket) => {
 
+  // Logging the primary server url
   if (primaryServerUrl == '') {
     console.log('Primary server not set');
   }
   else {
     console.log('Primary server set:', primaryServerUrl);
   }
+
+  // If the WebSocket instance has not been created, create a new WebSocket connection with the primary url
   if (!actualWebSocket) {
     console.log("NEW SERVER WEBSOCKET IS CREATED")
     actualWebSocket = new WebSocket(`${primaryServerUrl}/${documentNumber}`);
@@ -44,6 +44,7 @@ const connectToWebSocketServer = (documentNumber: string, ws: WebSocket) => {
     console.log(`[Server] Connected to actual WebSocket server for document number ${documentNumber}`);
   });
 
+  // Handle WebSocket message event
   actualWebSocket.on('message', (message: WebSocket.Data) => {
     console.log('[Server] Received message from actual WebSocket server:', message);
     // Forward the message to the client
@@ -52,14 +53,16 @@ const connectToWebSocketServer = (documentNumber: string, ws: WebSocket) => {
     }
   });
 
+  // Handle WebSocket error event
   actualWebSocket.on('error', (error) => {
     console.error('WebSocket error:', error);
     actualWebSocket.close();
-    // If the primary server fails, try connecting to the secondary server
+    // If the primary server fails, try connecting to the primary server
     console.log('Trying again server...');
     connectToWebSocketServer(documentNumber, ws);
   });
 
+  // Handle WebSocket close event
   actualWebSocket.on('close', () => {
     console.log(`[Server] Disconnected from actual WebSocket server for document number ${documentNumber}`);
   });
@@ -72,6 +75,7 @@ wsApp.ws('/:documentNumber', (ws: WebSocket, req: Request) => {
   // Create a WebSocket connection to the primary server
   connectToWebSocketServer(documentNumber, ws);
 
+  // Handle WebSocket open event
   ws.on('open', () => {
     console.log(`[client] WebSocket connection opened for document number ${documentNumber}`);
     ws.send('WebSocket connection opened to proxy server');
@@ -80,7 +84,7 @@ wsApp.ws('/:documentNumber', (ws: WebSocket, req: Request) => {
   // Forward messages received from the client to the actual WebSocket server
   ws.on('message', (message: WebSocket.Data) => {
     console.log('[client] Received message from client:', message);
-    // Forward the message to the WebSocket server
+    // Forward the message to the WebSocket server if the connection is open
     if (actualWebSocket.readyState === WebSocket.OPEN) {
       console.log('Forwarding message to actual WebSocket server:', message);
       actualWebSocket.send(message);
@@ -96,13 +100,13 @@ wsApp.ws('/:documentNumber', (ws: WebSocket, req: Request) => {
     console.log(`[client] WebSocket connection closed for document number ${documentNumber}`);
   });
 
+  // Handle WebSocket error event
   ws.onerror = (error) => {
     console.error('WebSocket error:', error);
   };
 });
 
 app.use(express.json(), cors());
-
 
 // Function to create a proxy middleware
 function createProxy(target: string): RequestHandler {
@@ -111,10 +115,9 @@ function createProxy(target: string): RequestHandler {
       const param = req.originalUrl;;
       return `${target}${param}`;
     },
-    proxyErrorHandler: function(err, res, next) {
+    proxyErrorHandler: function(err: Error, _, next) {
       console.error('Primary server error:', err);
-      console.error('Server is:', primaryServerUserUrl)
-      // If already switched to secondary and still error, proceed with error handling
+      console.error('Current Server URL is:', primaryServerUserUrl)
       return next(err);
     }
   });
@@ -122,16 +125,28 @@ function createProxy(target: string): RequestHandler {
 
 const proxyMiddleware = createProxy(primaryServerUserUrl);
 
+/**
+ * GET /api
+ * Proxy the request to the primary User Server
+ */
 app.use('/api', (req: Request, res: Response, next: NextFunction) => {
   console.log(req.originalUrl)
   console.log('Proxying request to primary server...');
   proxyMiddleware(req, res, next);
 }, cors());
 
-app.get("/server-down", (req: Request, res: Response) => {
+/**
+ * GET /server-down
+ * Simulate the primary server being down
+ */
+app.get("/server-down", (_, res: Response) => {
   res.status(500).send("Servers are down");
 });
 
+/**
+ * POST /primary-update
+ * Receive the update the primary server url from the Editor Server
+ */
 app.post("/primary-update", (req: Request, res: Response) => {
   const primary = req.body.id;
 
